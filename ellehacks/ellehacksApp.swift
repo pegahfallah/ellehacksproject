f0d313bc16d93a38a4cfb89541eb11f3c39dcf84
Pegah <43738935+pegahfallah@users.noreply.github.com>
2023-02-18 14:04:39 -0500

    Initial Commit

---
+++ ellehacks/ellehacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ellehacksApp.swift
+//  ellehacks
+//
+//  Created by Pegah Fallah on 2023-02-18.
+//
+
+import SwiftUI
+
+@main
+struct ellehacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
